239a5ca255053be58a9c1abae51ae1ec96c54a18
Varuas37 <saurav.panthee@gmail.com>
2022-02-01 18:28:12 -0600

    made changes to user model

--- src/auth/domain/User.ts
+++ src/auth/domain/User.ts
@@ -4,6 +4,5 @@ export default class User {
         public readonly username: string,
         public readonly email: string,
         public readonly password: string,
-        public readonly kitchenId?: string,
     ) { }
 }
