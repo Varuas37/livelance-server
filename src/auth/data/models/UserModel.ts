637f820268086c8f2d4c13088c4b26114c21395b
Varuas37 <saurav.panthee@gmail.com>
2022-02-11 22:32:17 -0600

    Work in progress

--- src/auth/data/models/UserModel.ts
+++ src/auth/data/models/UserModel.ts
@@ -5,12 +5,10 @@ export interface UserModel extends mongoose.Document {
     username: string
     email: string
     password?: string
-
 }
 
 export const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: String,
-
 })
