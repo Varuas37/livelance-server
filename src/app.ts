cbf6136e0319e8d669100831267d6f42d05362e1
cvam77 <schaudhary@patriots.uttyler.edu>
2022-03-28 19:05:58 -0500

    fixing the cors thing

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,23 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import CompositionRoot from './CompositionRoot'
+import dotenv from "dotenv";
+import express from "express";
+import CompositionRoot from "./CompositionRoot";
 
-dotenv.config()
-CompositionRoot.configure()
+dotenv.config();
+CompositionRoot.configure();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/auth', CompositionRoot.authRouter())
-app.use('/jobs', CompositionRoot.jobRouter())
-app.use('/profile', CompositionRoot.profileRouter())
-app.use('/message', CompositionRoot.messageRouter())
-app.use('/review', CompositionRoot.reviewRouter())
+app.use("/auth", CompositionRoot.authRouter());
+app.use("/jobs", CompositionRoot.jobRouter());
+app.use("/profile", CompositionRoot.profileRouter());
+app.use("/message", CompositionRoot.messageRouter());
+app.use("/review", CompositionRoot.reviewRouter());
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
